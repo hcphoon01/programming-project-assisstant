1d386f59279abf2e29a4698240966212dd3f0107
Harry Cameron <hcphoon01@gmail.com>
2019-04-02 09:07:15 +0100

    More boilerplate stuff

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 var {app, BrowserWindow} = require('electron');
+const { ipcMain } = require("electron");
 
-var mainWindow = null;
+var win = null;
 
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800
+    win = new BrowserWindow({
+        height: 1080,
+        width: 1920,
+        webPreferences: {
+            nodeIntegration: true,
+            allowEval: false
+        }
     });
+    win.maximize();
+    win.loadURL('file://' + __dirname + '/app/landing.html');
+    win.webContents.toggleDevTools();
+});
 
-    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+ipcMain.on("changeWindow", function(event, arg) {
+    switch (arg) {
+        case "index":
+            win.loadURL('file://' + __dirname + '/app/index.html');
+            break;
+    }
 });
